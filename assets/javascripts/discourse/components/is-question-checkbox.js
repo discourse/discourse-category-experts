8a0d3102826d01bb619b22f5c20e2d1fdc646149
Mark VanLandingham <markvanlan@gmail.com>
2021-03-12 13:48:26 -0600

    cleanup

--- assets/javascripts/discourse/components/is-question-checkbox.js
+++ assets/javascripts/discourse/components/is-question-checkbox.js
@@ -1,9 +1,3 @@
-import discourseComputed from "discourse-common/utils/decorators";
 import Component from "@ember/component";
 
-export default Component.extend({
-  init() {
-    this._super(...arguments);
-    console.log(this.model);
-  },
-});
+export default Component.extend({});
