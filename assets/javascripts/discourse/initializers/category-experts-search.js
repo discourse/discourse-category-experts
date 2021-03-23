52b109b93db3641748f7ab6cc85a8a767204e5de
Mark VanLandingham <markvanlan@gmail.com>
2021-03-23 11:51:01 -0500

    Hide UI based on staff or category expert

--- assets/javascripts/discourse/initializers/category-experts-search.js
+++ assets/javascripts/discourse/initializers/category-experts-search.js
@@ -70,6 +70,16 @@ function initialize(api) {
     "advanced-search-options-below",
     "category-experts-search-fields",
     {
+      shouldRender(args, component) {
+        return (
+          component.siteSettings.enable_category_experts
+        );
+      },
+
+      setupComponent() {
+        this.set("canSeeIsQuestionFilter", (this.currentUser.staff || (this.currentUser.expert_for_category_ids && this.currentUser.expert_for_category_ids.length)));
+      },
+
       actions: {
         onChangeCheckBox(path, fn, event) {
           this.onChangeSearchedTermField(path, fn, event.target.checked);
