9334e1c646e616473c37bf4c965dbc493d7e718b
Mark VanLandingham <markvanlan@gmail.com>
2021-03-24 11:58:10 -0500

    prettier

--- assets/javascripts/discourse/components/is-question-checkbox.js
+++ assets/javascripts/discourse/components/is-question-checkbox.js
@@ -14,6 +14,6 @@ export default Component.extend({
   show(model, category) {
     if (!category || !category.allowingCategoryExpertQuestions) return false;
 
-    return model.editingFirstPost || model.creatingTopic
-  }
+    return model.editingFirstPost || model.creatingTopic;
+  },
 });
