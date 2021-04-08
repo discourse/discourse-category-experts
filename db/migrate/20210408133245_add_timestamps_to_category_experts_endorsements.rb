63c3d817904013295f4da9b32079659c770d237f
Mark VanLandingham <markvanlan@gmail.com>
2021-04-08 08:36:18 -0500

    Add timestamps to category expert endorsements table

---
+++ db/migrate/20210408133245_add_timestamps_to_category_experts_endorsements.rb
@@ -0,0 +1,5 @@
+class AddTimestampsToCategoryExpertsEndorsements < ActiveRecord::Migration[6.0]
+  def change
+    add_timestamps :category_expert_endorsements, null: false, default: -> { 'NOW()' }
+  end
+end
