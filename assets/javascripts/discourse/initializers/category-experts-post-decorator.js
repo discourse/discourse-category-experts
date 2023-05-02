2ed6e9badd320f29b2a84c87be9088e0e718a5d7
Isaac Janzen <isaac.janzen@discourse.org>
2023-05-02 09:10:14 -0500

    Use sitesettings service

--- assets/javascripts/discourse/initializers/category-experts-post-decorator.js
+++ assets/javascripts/discourse/initializers/category-experts-post-decorator.js
@@ -31,10 +31,9 @@ function setPostCategoryExpertAttributes(
 }
 
 function initializeWithApi(api) {
-  const requiresApproval =
-    api.container.lookup(
-      "site-settings:main"
-    ).category_experts_posts_require_approval;
+  const requiresApproval = api.container.lookup(
+    "service:site-settings"
+  ).category_experts_posts_require_approval;
 
   if (requiresApproval) {
     const appEvents = api.container.lookup("service:app-events");
