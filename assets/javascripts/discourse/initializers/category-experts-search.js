a391c3c361b19d3a12486029e068a994ae2e6a74
David Taylor <david@taylorhq.com>
2023-11-23 14:00:51 +0000

    DEV: Resolve registerConnectorClass deprecation

--- assets/javascripts/discourse/initializers/category-experts-search.js
+++ assets/javascripts/discourse/initializers/category-experts-search.js
@@ -80,32 +80,6 @@ function initialize(api) {
       );
     },
   });
-
-  api.registerConnectorClass(
-    "advanced-search-options-below",
-    "category-experts-search-fields",
-    {
-      shouldRender(args, component) {
-        return component.siteSettings.enable_category_experts;
-      },
-
-      setupComponent() {
-        this.set(
-          "canSeeIsQuestionFilter",
-          this.currentUser &&
-            (this.currentUser.staff ||
-              (this.currentUser.expert_for_category_ids &&
-                this.currentUser.expert_for_category_ids.length))
-        );
-      },
-
-      actions: {
-        onChangeCheckBox(path, fn, event) {
-          this.onChangeSearchedTermField(path, fn, event.target.checked);
-        },
-      },
-    }
-  );
 }
 
 export default {
