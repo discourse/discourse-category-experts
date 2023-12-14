59c51423d895ed0c97f5da5b8e86327e543c300b
Daniel Waterworth <me@danielwaterworth.com>
2023-12-14 12:41:36 -0600

    FIX: Fix usage of boolean custom field CATEGORY_ACCEPTING_ENDORSEMENTS

--- spec/system/reviewable_category_expert_suggestion_spec.rb
+++ spec/system/reviewable_category_expert_suggestion_spec.rb
@@ -13,7 +13,7 @@ describe "Reviewables - Category expert suggestion", type: :system, js: true do
 
     sign_in(current_user)
     category.custom_fields[CategoryExperts::CATEGORY_EXPERT_GROUP_IDS] = group.id.to_s
-    category.custom_fields[CategoryExperts::CATEGORY_ACCEPTING_ENDORSEMENTS] = "true"
+    category.custom_fields[CategoryExperts::CATEGORY_ACCEPTING_ENDORSEMENTS] = true
     category.save!
   end
 
