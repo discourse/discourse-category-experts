4174b55fcd940e7298c57ff0c969d0fa7b059c3f
Sérgio Saquetim <1108771+megothss@users.noreply.github.com>
2024-11-12 15:47:11 -0300

    DEV: Added compatibility with the Glimmer Post Menu (#171)

---
+++ assets/javascripts/discourse/components/category-experts-approve-button.gjs
@@ -0,0 +1,79 @@
+import Component from "@glimmer/component";
+import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
+import DButton from "discourse/components/d-button";
+import { ajax } from "discourse/lib/ajax";
+import { popupAjaxError } from "discourse/lib/ajax-error";
+
+export default class CategoryExpertsApproveButton extends Component {
+  static shouldRender(args) {
+    return (
+      !args.post.category_expert_approved_group &&
+      args.post.needs_category_expert_approval
+    );
+  }
+
+  // TODO (glimmer-post-menu): Remove this static method and move the code into the button action after the widget code is removed
+  static approveCategoryExpertPost(post, appEvents) {
+    setPostCategoryExpertAttributes(post, appEvents, {
+      approved: true,
+    });
+  }
+
+  @service appEvents;
+
+  get showLabel() {
+    return this.args.showLabel ?? true;
+  }
+
+  @action
+  approveCategoryExpertPost() {
+    CategoryExpertsApproveButton.approveCategoryExpertPost(
+      this.args.post,
+      this.appEvents
+    );
+  }
+
+  <template>
+    <DButton
+      class="approve-category-expert-post"
+      ...attributes
+      @action={{this.approveCategoryExpertPost}}
+      @icon="thumbs-up"
+      @label={{if this.showLabel "category_experts.approve"}}
+      @title="category_experts.approve"
+    />
+  </template>
+}
+
+export async function setPostCategoryExpertAttributes(
+  post,
+  appEvents,
+  opts = { approved: true }
+) {
+  try {
+    const response = await ajax(
+      `/category-experts/${opts.approved ? "approve" : "unapprove"}`,
+      {
+        type: "POST",
+        data: { post_id: post.id },
+      }
+    );
+
+    post.setProperties({
+      needs_category_expert_approval: !opts.approved,
+      category_expert_approved_group: opts.approved
+        ? response.group_name
+        : false,
+    });
+
+    post.topic.setProperties({
+      needs_category_expert_post_approval:
+        response.topic_needs_category_expert_approval,
+      expert_post_group_names: response.topic_expert_post_group_names,
+    });
+    appEvents.trigger("post-stream:refresh", { id: post.id });
+  } catch (error) {
+    popupAjaxError(error);
+  }
+}
--- assets/javascripts/discourse/initializers/category-experts-post-decorator.js
+++ assets/javascripts/discourse/initializers/category-experts-post-decorator.js
@@ -1,34 +1,9 @@
 import { next } from "@ember/runloop";
-import { ajax } from "discourse/lib/ajax";
-import { popupAjaxError } from "discourse/lib/ajax-error";
 import { withPluginApi } from "discourse/lib/plugin-api";
+import { withSilencedDeprecations } from "discourse-common/lib/deprecated";
 import { iconNode } from "discourse-common/lib/icon-library";
-
-function setPostCategoryExpertAttributes(
-  post,
-  appEvents,
-  opts = { approved: true }
-) {
-  ajax(`/category-experts/${opts.approved ? "approve" : "unapprove"}`, {
-    type: "POST",
-    data: { post_id: post.id },
-  })
-    .then((response) => {
-      post.setProperties({
-        needs_category_expert_approval: !opts.approved,
-        category_expert_approved_group: opts.approved
-          ? response.group_name
-          : false,
-      });
-      post.topic.setProperties({
-        needs_category_expert_post_approval:
-          response.topic_needs_category_expert_approval,
-        expert_post_group_names: response.topic_expert_post_group_names,
-      });
-      appEvents.trigger("post-stream:refresh", { id: post.id });
-    })
-    .catch(popupAjaxError);
-}
+import CategoryExpertsApproveButton from "../components/category-experts-approve-button";
+import CategoryExpertsUnapproveButton from "../components/category-experts-unapprove-button";
 
 function initializeWithApi(api) {
   const requiresApproval = api.container.lookup(
@@ -36,56 +11,13 @@ function initializeWithApi(api) {
   ).category_experts_posts_require_approval;
 
   if (requiresApproval) {
-    const appEvents = api.container.lookup("service:app-events");
-
     api.includePostAttributes(
       "needs_category_expert_approval",
       "category_expert_approved_group",
       "can_manage_category_expert_posts"
     );
 
-    api.addPostMenuButton("category-expert-post-approval", (attrs) => {
-      if (!attrs.can_manage_category_expert_posts) {
-        return;
-      }
-
-      if (
-        attrs.needs_category_expert_approval &&
-        !attrs.category_expert_approved_group
-      ) {
-        return {
-          action: "approveCategoryExpertPost",
-          icon: "thumbs-up",
-          className: "approve-category-expert-post",
-          title: "category_experts.approve",
-          label: "category_experts.approve",
-          position: "first",
-        };
-      } else if (
-        attrs.category_expert_approved_group &&
-        !attrs.needs_category_expert_approval
-      ) {
-        return {
-          action: "unapproveCategoryExpertPost",
-          icon: "thumbs-down",
-          className: "unapprove-category-expert-post",
-          title: "category_experts.unapprove",
-          position: "second-last-hidden",
-        };
-      }
-    });
-
-    api.attachWidgetAction("post", "approveCategoryExpertPost", function () {
-      setPostCategoryExpertAttributes(this.model, appEvents, {
-        approved: true,
-      });
-    });
-
-    api.attachWidgetAction("post", "unapproveCategoryExpertPost", function () {
-      setPostCategoryExpertAttributes(this.model, appEvents, {
-        approved: false,
-      });
-    });
+    customizePostMenu(api);
   }
 
   api.decorateWidget("post:after", (helper) => {
@@ -120,11 +52,96 @@ function initializeWithApi(api) {
   });
 }
 
+function customizePostMenu(api) {
+  const transformerRegistered = api.registerValueTransformer(
+    "post-menu-buttons",
+    ({
+      value: dag,
+      context: {
+        post,
+        firstButtonKey,
+        lastHiddenButtonKey,
+        secondLastHiddenButtonKey,
+      },
+    }) => {
+      if (!post.can_manage_category_expert_posts) {
+        return;
+      }
+
+      dag.add("category-expert-approve-post", CategoryExpertsApproveButton, {
+        before: firstButtonKey,
+      });
+
+      dag.add(
+        "category-expert-unapprove-post",
+        CategoryExpertsUnapproveButton,
+        {
+          before: lastHiddenButtonKey,
+          after: secondLastHiddenButtonKey,
+        }
+      );
+    }
+  );
+
+  const silencedKey =
+    transformerRegistered && "discourse.post-menu-widget-overrides";
+
+  withSilencedDeprecations(silencedKey, () => customizeWidgetPostMenu(api));
+}
+
+function customizeWidgetPostMenu(api) {
+  api.addPostMenuButton("category-expert-post-approval", (attrs) => {
+    if (!attrs.can_manage_category_expert_posts) {
+      return;
+    }
+
+    if (
+      attrs.needs_category_expert_approval &&
+      !attrs.category_expert_approved_group
+    ) {
+      return {
+        action: "approveCategoryExpertPost",
+        icon: "thumbs-up",
+        className: "approve-category-expert-post",
+        title: "category_experts.approve",
+        label: "category_experts.approve",
+        position: "first",
+      };
+    } else if (
+      attrs.category_expert_approved_group &&
+      !attrs.needs_category_expert_approval
+    ) {
+      return {
+        action: "unapproveCategoryExpertPost",
+        icon: "thumbs-down",
+        className: "unapprove-category-expert-post",
+        title: "category_experts.unapprove",
+        position: "second-last-hidden",
+      };
+    }
+  });
+
+  const appEvents = api.container.lookup("service:app-events");
+  api.attachWidgetAction("post", "approveCategoryExpertPost", function () {
+    CategoryExpertsApproveButton.approveCategoryExpertPost(
+      this.model,
+      appEvents
+    );
+  });
+
+  api.attachWidgetAction("post", "unapproveCategoryExpertPost", function () {
+    CategoryExpertsUnapproveButton.unapproveCategoryExpertPost(
+      this.model,
+      appEvents
+    );
+  });
+}
+
 export default {
   name: "discourse-experts-post-decorator",
 
   initialize() {
-    withPluginApi("0.10.1", (api) => {
+    withPluginApi("1.34.0", (api) => {
       initializeWithApi(api);
     });
   },
